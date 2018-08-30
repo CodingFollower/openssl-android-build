31d896b61eee8b3021bd66e26fc057d0c81e4bc3
2coding <CodingApe@outlook.com>
2018-08-30 16:50:47 +0800

    lastest version

--- README.md
+++ README.md
@@ -2,28 +2,26 @@
 
 Requirement
 ---
+* `clang`
 * `android-ndk-r16b`
-* `openssl-1.0.1g`
+* `openssl-1.1.0i`
 
 Command
 ---
 * set `ANDROID_NDK_ROOT`
-* make toolchain
 
-        $ANDROID_NDK_ROOT/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-14 --install-dir=/tmp/arm-android-toolchain
+        export ANDROID_NDK_ROOT=...
 * set env
   
-        rm -fR openssl-OpenSSL_1_0_1g
-        chmod a+x setenv-android-xxx.sh
-        tar xzf openssl-1.0.1g.tar.gz
-        . ./setenv-android-xxx.sh
+        rm -fR openssl-1.1.0i
+        tar xzf openssl-1.1.0i.tar.gz
+        chmod a+x setenv-android.sh
+        . ./setenv-android.sh
 * make static lib
   
-        cd openssl-OpenSSL_1_0_1g
-        ./config no-shared no-ssl2 no-ssl3 no-hw no-engine no-asm --openssldir=/tmp/openssl-android-armv7-build
-        make depend
-        make all
-
+        cd openssl-1.1.0i
+        ./config no-shared no-ssl2 no-ssl3 no-hw no-engine no-asm  --openssldir=/tmp/openssl-android-armv7-build
+        make build_libs
 * what you get
 
         rm -fR ../build
--- setenv-android.sh
+++ setenv-android.sh
@@ -1,50 +1,36 @@
 #!/bin/bash
 
-#ANDROID_NDK_ROOT="~/Library/Android/ndk/android-ndk-r16b"
-
-_STANDALONE_TOOLCHAIN_ROOT="/tmp/arm-android-toolchain"
 _ARM_ARCH_PREFIX="arm-linux-androideabi"
-_ANDROID_EABI="${_ARM_ARCH_PREFIX}-4.9"
 _ANDROID_ARCH="arch-arm"
 _ANDROID_API="android-14"
 
 #####################################################################
+#standalone-toolchain
+_STANDALONE_TOOLCHAIN_ROOT=/tmp/arm-android-toolchain
+if [ -d $_STANDALONE_TOOLCHAIN_ROOT ]; then
+    rm -fR $_STANDALONE_TOOLCHAIN_ROOT
+fi
+
+$ANDROID_NDK_ROOT/build/tools/make-standalone-toolchain.sh --arch=arm --platform=$_ANDROID_API --install-dir=$_STANDALONE_TOOLCHAIN_ROOT
 # Error checking
 if [ ! -d "${_STANDALONE_TOOLCHAIN_ROOT}" ]; then
   echo "Error: $_STANDALONE_TOOLCHAIN_ROOT is not a valid path. Please edit this script."
-  # exit 1
 fi
 
-# Error checking
-#if [ ! -d "$_STANDALONE_TOOLCHAIN_ROOT/toolchains/$_ANDROID_EABI" ]; then
-#  echo "Error: ANDROID_EABI is not a valid path. Please edit this script."
-#  # echo "$_STANDALONE_TOOLCHAIN_ROOT/toolchains/$_ANDROID_EABI"
-#  exit 1
-#fi
-
+#####################################################################
+#toolchain
 ANDROID_TOOLCHAIN="$_STANDALONE_TOOLCHAIN_ROOT/bin"
-
-#for host in "linux-x86_64" "linux-x86" "darwin-x86_64" "darwin-x86"
-#do
-#  if [ -d "$_STANDALONE_TOOLCHAIN_ROOT/toolchains/$_ANDROID_EABI/prebuilt/$host/bin" ]; then
-#    ANDROID_TOOLCHAIN="$_STANDALONE_TOOLCHAIN_ROOT/toolchains/$_ANDROID_EABI/prebuilt/$host/bin"
-#    break
-#  fi
-#done
-
 # Error checking
 if [ -z "$ANDROID_TOOLCHAIN" ] || [ ! -d "$ANDROID_TOOLCHAIN" ]; then
   echo "Error: $ANDROID_TOOLCHAIN is not valid. Please edit this script."
-  # echo "$ANDROID_TOOLCHAIN"
-  # exit 1
 fi
 
 case $_ANDROID_ARCH in
 	arch-arm)	  
-      ANDROID_TOOLS="arm-linux-androideabi-gcc arm-linux-androideabi-ranlib arm-linux-androideabi-ld"
+      ANDROID_TOOLS="$_ARM_ARCH_PREFIX-gcc $_ARM_ARCH_PREFIX-ranlib $_ARM_ARCH_PREFIX-ld"
 	  ;;
 	arch-x86)	  
-      ANDROID_TOOLS="i686-linux-android-gcc i686-linux-android-ranlib i686-linux-android-ld"
+      ANDROID_TOOLS="$_ARM_ARCH_PREFIX-gcc $_ARM_ARCH_PREFIX-ranlib $_ARM_ARCH_PREFIX-ld"
 	  ;;	  
 	*)
 	  echo "ERROR ERROR ERROR"
@@ -56,8 +42,6 @@ do
   # Error checking
   if [ ! -e "$ANDROID_TOOLCHAIN/$tool" ]; then
     echo "Error: Failed to find $tool. Please edit this script."
-    # echo "$ANDROID_TOOLCHAIN/$tool"
-    # exit 1
   fi
 done
 
@@ -67,7 +51,17 @@ if [ ! -z "$ANDROID_TOOLCHAIN" ]; then
 fi
 
 #####################################################################
+#sysroot
+ANDROID_SYSROOT="$_STANDALONE_TOOLCHAIN_ROOT/sysroot"
+# Error checking
+if [ -z "$ANDROID_SYSROOT" ] || [ ! -d "$ANDROID_SYSROOT" ]; then
+  echo "Error: ANDROID_SYSROOT is not valid. Please edit this script."
+fi
 
+export CROSS_SYSROOT="$ANDROID_SYSROOT"
+#export ANDROID_DEV="$ANDROID_SYSROOT/usr"
+
+#####################################################################
 # Most of these should be OK (MACHINE, SYSTEM, ARCH). RELEASE is ignored.
 export MACHINE=armv7
 export RELEASE=2.6.37
@@ -80,50 +74,20 @@ if [ "$_ANDROID_ARCH" == "arch-x86" ]; then
 	export RELEASE=2.6.37
 	export SYSTEM=android
 	export ARCH=x86
-	export CROSS_COMPILE="i686-linux-android-"
+	# export CROSS_COMPILE="i686-linux-android-"
 fi
 
-# For the Android toolchain
-# https://android.googlesource.com/platform/ndk/+/ics-mr0/docs/STANDALONE-TOOLCHAIN.html
-#export ANDROID_SYSROOT="$ANDROID_NDK_ROOT/platforms/$_ANDROID_API/$_ANDROID_ARCH"
-export ANDROID_SYSROOT="$_STANDALONE_TOOLCHAIN_ROOT/sysroot"
-
-# Error checking
-if [ -z "$ANDROID_SYSROOT" ] || [ ! -d "$ANDROID_SYSROOT" ]; then
-  echo "Error: ANDROID_SYSROOT is not valid. Please edit this script."
-  # echo "$ANDROID_SYSROOT"
-  # exit 1
-fi
-
-export CROSS_SYSROOT="$ANDROID_SYSROOT"
-export NDK_SYSROOT="$ANDROID_SYSROOT"
-export SYSROOT="$ANDROID_SYSROOT"
-export ANDROID_NDK_SYSROOT="$ANDROID_SYSROOT"
-export ANDROID_API="$_ANDROID_API"
-
-# CROSS_COMPILE and ANDROID_DEV are DFW (Don't Fiddle With). Its used by OpenSSL build system.
-# export CROSS_COMPILE="arm-linux-androideabi-"
-#export ANDROID_DEV="$ANDROID_NDK_ROOT/platforms/$_ANDROID_API/$_ANDROID_ARCH/usr"
-export ANDROID_DEV="$ANDROID_SYSROOT/usr"
-#export ANDROID_C_ROOT="$ANDROID_NDK_ROOT/sysroot/usr"
-# ANDROID_ASM_ROOT="${ANDROID_C_ROOT}/include/${_ARM_ARCH_PREFIX}"
-export HOSTCC=gcc
-export RANLIB=$ANDROID_TOOLCHAIN/arm-linux-androideabi-ranlib
-#export CFLAG="-DOPENSSL_THREADS -D_REENTRANT -DDSO_DLFCN -DHAVE_DLFCN_H -march=armv7-a -mandroid -I${ANDROID_C_ROOT}/include -I${ANDROID_ASM_ROOT} -B${ANDROID_DEV}/lib -O3 -fomit-frame-pointer -Wall"
+export RANLIB=$ANDROID_TOOLCHAIN/${CROSS_COMPILE}ranlib
 
 VERBOSE=1
 if [ ! -z "$VERBOSE" ] && [ "$VERBOSE" != "0" ]; then
-  #echo "ANDROID_NDK_ROOT: $ANDROID_NDK_ROOT"
   echo "ANDROID_ARCH: $_ANDROID_ARCH"
-  echo "ANDROID_EABI: $_ANDROID_EABI"
-  echo "ANDROID_API: $ANDROID_API"
-  echo "ANDROID_SYSROOT: $ANDROID_SYSROOT"
+  #echo "ANDROID_EABI: $_ANDROID_EABI"
+  echo "ANDROID_API: $_ANDROID_API"
+  echo "CROSS_SYSROOT: $CROSS_SYSROOT"
   echo "ANDROID_TOOLCHAIN: $ANDROID_TOOLCHAIN"
-  echo "FIPS_SIG: $FIPS_SIG"
+  #echo "FIPS_SIG: $FIPS_SIG"
   echo "CROSS_COMPILE: $CROSS_COMPILE"
-  echo "ANDROID_DEV: $ANDROID_DEV"
-  #echo "ANDROID_C_ROOT: $ANDROID_C_ROOT"
-  #echo "ANDROID_ASM_ROOT: $ANDROID_ASM_ROOT"
+  #echo "ANDROID_DEV: $ANDROID_DEV"
   echo "RANLIB: $RANLIB"
-#  echo "CFLAG: $CFLAG"
 fi
